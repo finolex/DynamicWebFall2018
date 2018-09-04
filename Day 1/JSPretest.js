bbe2e5d648a54b93ae6e7cd1b2418afa36a346a1
Abhiroop CVK <abhiroopcvk@10-18-116-147.dynapool.wireless.nyu.edu>
2018-09-04 14:21:44 -0400

    day 1 class

---
+++ Day 1/JSPretest.js
@@ -0,0 +1,21 @@
+name = "this is my name";
+
+
+adder = function(x) {
+	new1 = x + 2 - 5
+	return new1;
+}
+
+console.log(adder(10));
+
+for(var i = 1; i < 11; i++) {
+	console.log(i);
+}
+
+
+birth_year = 1996
+future_year = 2050
+possible1 = future_year - birth_year
+possible2 = possible1 - 1
+
+console.log('I\'ll be either', possible2,' or ', possible1, ' in', future_year)

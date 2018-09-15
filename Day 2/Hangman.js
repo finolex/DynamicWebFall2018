691678092baca7b314c9b6273da121d8dc18f97c
Abhiroop CVK <abhiroopcvk@Abhiroops-MBP.fios-router.home>
2018-09-15 18:29:28 -0400

    made changes to the hangman game to pull from a JSON of random words

--- Day 2/Hangman.js
+++ Day 2/Hangman.js
@@ -1,7 +1,76 @@
-wordArray = ['f', 'o', 'x', 'o'];
+// copied a JSON colors file
+
+var wordList = {
+  "colors": [
+    {
+      "color": "black",
+      "category": "hue",
+      "type": "primary",
+      "code": {
+        "rgba": [255,255,255,1],
+        "hex": "#000"
+      }
+    },
+    {
+      "color": "white",
+      "category": "value",
+      "code": {
+        "rgba": [0,0,0,1],
+        "hex": "#FFF"
+      }
+    },
+    {
+      "color": "red",
+      "category": "hue",
+      "type": "primary",
+      "code": {
+        "rgba": [255,0,0,1],
+        "hex": "#FF0"
+      }
+    },
+    {
+      "color": "blue",
+      "category": "hue",
+      "type": "primary",
+      "code": {
+        "rgba": [0,0,255,1],
+        "hex": "#00F"
+      }
+    },
+    {
+      "color": "yellow",
+      "category": "hue",
+      "type": "primary",
+      "code": {
+        "rgba": [255,255,0,1],
+        "hex": "#FF0"
+      }
+    },
+    {
+      "color": "green",
+      "category": "hue",
+      "type": "secondary",
+      "code": {
+        "rgba": [0,255,0,1],
+        "hex": "#0F0"
+      }
+    },
+  ]
+}
+
+//declare a random number between 0 and 5 since there are 6 colors/words available in the JSON above
+let randomSelection = Math.floor(Math.random() * 6);
+let randomColor = wordList.colors[randomSelection].color;
+wordArray = [];
+
+//pushes every individual letter of randomColor into the wordArray
+for(let i = 0; i < randomColor.length; i++){
+  wordArray.push(randomColor[i]);
+}
+
 guessedArray = [];
 lettersUsed = [];
-var life = 6;
+var life = 8;
 
 for(var i = 0; i< wordArray.length; i++){
   guessedArray.push('_');
@@ -40,122 +109,131 @@ function guessLetter(newGuess){
       //check against placeholder if updates have been made, and give remark to the user accordingly
       if (JSON.stringify(guessedArray) === JSON.stringify(placeholderArray)){
         console.log('You guessed a wrong letter.');
+        console.log(" ");
         life -= 1;
       } else {
-        console.log('You guessed a correct letter!')
+        console.log('You guessed a correct letter!');
+        console.log(" ");
       }
 
       //check if user finished guessing
       if (JSON.stringify(guessedArray) === JSON.stringify(wordArray)){
         console.log('You finished!');
+        console.log("The full word is", randomColor)
       } else {
-        console.log('Keep going :)')
-      }
+        console.log('Keep going :)');
+        console.log(" ");
+        //print to user updates at the end of every guess
+        console.log("This is where you're at", guessedArray, "and you've ", life, 'lives left. These are the letters you\'ve guessed before ', lettersUsed);
+        console.log('');
 
-      //print to user updates at the end of every guess
-      console.log("This is where you're at", guessedArray, "and you've ", life, 'lives left. These are the letters you\'ve guessed before ', lettersUsed);
-      console.log('');
-
-      if(life == 6){
-        console.log("    0000000000000");
-        console.log("0           0");
-        console.log("0    ");
-        console.log("0    ");
-        console.log("0  ");
-        console.log("0   ");
-        console.log("0   ");
-        console.log("0  ");
-        console.log("0  ");
-        console.log("0  ");
-        console.log("0 ");
-        console.log("0  ");
-        console.log("0");
-        console.log("0");
-        console.log("0");
-      } else if (life == 5){
-        console.log("    0000000000000");
-        console.log("0           0");
-        console.log("0           1");
-        console.log("0          1 1");
-        console.log("0           1");
-        console.log("0  ");
-        console.log("0     ");
-        console.log("0      ");
-        console.log("0    ");
-        console.log("0     ");
-        console.log("0  ");
-        console.log("0");
-        console.log("0");
-        console.log("0");
-        console.log("0");
-      } else if(life == 4){
-        console.log("    0000000000000");
-        console.log("0           0");
-        console.log("0           1");
-        console.log("0          1 1");
-        console.log("0           1");
-        console.log("0           2");
-        console.log("0           2 ");
-        console.log("0           2  ");
-        console.log("0        ");
-        console.log("0     ");
-        console.log("0        ");
-        console.log("0    ");
-        console.log("0");
-        console.log("0");
-        console.log("0");
-      } else if (life == 3){
-      console.log("    0000000000000");
-      console.log("0           0");
-      console.log("0           1");
-      console.log("0          1 1");
-      console.log("0           1");
-      console.log("0          32");
-      console.log("0         3 2 3");
-      console.log("0        3  2  3");
-      console.log("0     ");
-      console.log("0        ");
-      console.log("0     ");
-      console.log("0      ");
-      console.log("0");
-      console.log("0");
-      console.log("0");
-      } else if (life == 2){
+        if(life >= 6){
           console.log("    0000000000000");
-      console.log("0           0");
-      console.log("0           1");
-      console.log("0          1 1");
-      console.log("0           1");
-      console.log("0          324");
-      console.log("0         3 2 3");
-      console.log("0        3  2  3");
-      console.log("0          4 ");
-      console.log("0         4   ");
-      console.log("0        4     ");
-      console.log("0       4       ");
-      console.log("0");
-      console.log("0");
-      console.log("0");
+          console.log("0           0");
+          console.log("0    ");
+          console.log("0    ");
+          console.log("0  ");
+          console.log("0   ");
+          console.log("0   ");
+          console.log("0  ");
+          console.log("0  ");
+          console.log("0  ");
+          console.log("0 ");
+          console.log("0  ");
+          console.log("0");
+          console.log("0");
+          console.log("0");
+          console.log(" ");
+        } else if (life == 5){
+          console.log("    0000000000000");
+          console.log("0           0");
+          console.log("0           1");
+          console.log("0          1 1");
+          console.log("0           1");
+          console.log("0  ");
+          console.log("0     ");
+          console.log("0      ");
+          console.log("0    ");
+          console.log("0     ");
+          console.log("0  ");
+          console.log("0");
+          console.log("0");
+          console.log("0");
+          console.log("0");
+          console.log(" ");
+        } else if(life == 4){
+          console.log("    0000000000000");
+          console.log("0           0");
+          console.log("0           1");
+          console.log("0          1 1");
+          console.log("0           1");
+          console.log("0           2");
+          console.log("0           2 ");
+          console.log("0           2  ");
+          console.log("0        ");
+          console.log("0     ");
+          console.log("0        ");
+          console.log("0    ");
+          console.log("0");
+          console.log("0");
+          console.log("0");
+          console.log(" ");
+        } else if (life == 3){
+          console.log("    0000000000000");
+          console.log("0           0");
+          console.log("0           1");
+          console.log("0          1 1");
+          console.log("0           1");
+          console.log("0          32");
+          console.log("0         3 2 3");
+          console.log("0        3  2  3");
+          console.log("0     ");
+          console.log("0        ");
+          console.log("0     ");
+          console.log("0      ");
+          console.log("0");
+          console.log("0");
+          console.log("0");
+          console.log(" ");
+        } else if (life == 2){
+          console.log("    0000000000000");
+          console.log("0           0");
+          console.log("0           1");
+          console.log("0          1 1");
+          console.log("0           1");
+          console.log("0          324");
+          console.log("0         3 2 3");
+          console.log("0        3  2  3");
+          console.log("0          4 ");
+          console.log("0         4   ");
+          console.log("0        4     ");
+          console.log("0       4       ");
+          console.log("0");
+          console.log("0");
+          console.log("0");
+          console.log(" ");
+        } else {
+        console.log("You've no more lives left.");
+        console.log("    0000000000000");
+        console.log("0           0");
+        console.log("0           1");
+        console.log("0          1 1");
+        console.log("0           1");
+        console.log("0          324");
+        console.log("0         3 2 4");
+        console.log("0        3  2  4");
+        console.log("0          4 5");
+        console.log("0         4   5");
+        console.log("0        4     5");
+        console.log("0       4       5");
+        console.log("0");
+        console.log("0");
+        console.log("0");
+        console.log(" ");
+        }
       }
-
-    } else {
-      console.log("You've no more lives left.");
-      console.log("    0000000000000");
-      console.log("0           0");
-      console.log("0           1");
-      console.log("0          1 1");
-      console.log("0           1");
-      console.log("0          324");
-      console.log("0         3 2 4");
-      console.log("0        3  2  4");
-      console.log("0          4 5");
-      console.log("0         4   5");
-      console.log("0        4     5");
-      console.log("0       4       5");
-      console.log("0");
-      console.log("0");
-      console.log("0");
     }
+      
   }
 }
 

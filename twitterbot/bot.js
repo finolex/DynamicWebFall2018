5230504a8b45d9e664e7369e9289b59cac549711
finolex <ac5847@nyu.edu>
2018-10-04 14:21:39 -0400

    configured twitter bot

--- twitterbot/bot.js
+++ twitterbot/bot.js
@@ -4,12 +4,32 @@ var config = require('./config');
 
 var T = new Twit(config);
 
-console.log('hi')
-
-T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-  console.log(data)
-})
 
 // T.get('search/tweets', { q: 'banana since:2011-07-11', count: 10 }, function(err, data, response) {
 //   console.log(data)
 // })
+
+function tweets(){
+	var costume = ["ghost", "the nun", "witch", "pumpking", "vampire"];
+	var quote = 'This is a bot that posts suggestions for halloween costumes. You should be a ' + costume[Math.floor(Math.random() * costume.length)];
+
+	var tweet = {
+		status: quote	
+	}
+
+	return tweet;
+}
+
+
+function didItTweet(err, data, response) {
+	if (err){
+		console.log(err + "it didn't work");
+	} else {
+		console.log('Worked!');
+	}
+}
+
+T.post('statuses/update', tweets(), didItTweet());
+
+
+

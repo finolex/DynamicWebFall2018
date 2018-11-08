a28da7cb3cb3c26465e2d16abc71c3c26bde17bf
finolex <ac5847@nyu.edu>
2018-11-08 11:29:26 -0500

    added login page for number trivia app but the check for whether account exists doesnt work yet

--- Day 16/app.js
+++ Day 16/app.js
@@ -1,17 +1,56 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-const app = express()
+const app = express();
+const fs = require('fs');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
+
+let loginsData = fs.readFileSync('logins.json');
+let logins = JSON.parse(loginsData);
+console.log(logins);
+
+//Code for the login page
+
 app.get('/', function (req, res) {
-  res.render('index', {triviaPost: null, error: null});
+  res.render('login');
 })
 
 app.post('/', function (req, res) {
+  let user = req.body.user;
+  let pass = req.body.pass;
+  let proceed = false;
+
+  if(user != ''){
+  	if(pass != ''){
+  		if (user == logins[String(user)]){
+	  	res.send("This username already exists. Please refresh page and try again");
+	 	} else {
+  		logins[user] = {"user": user, "pass": pass}
+
+		  fs.writeFileSync('logins.json', JSON.stringify(logins, null, 2), function(err){
+		  	if (err) throw err;
+		  	console.log("Saved Login!");
+		  });
+
+		  res.redirect('/app');
+  		}
+  	} else {res.send('Type in a password. Please refresh page and try again')}
+  } else {res.send('Type in a username. Please refresh page and try again')}
+})
+
+
+
+//Code for the app itself
+
+app.get('/app', function (req, res) {
+  res.render('index', {triviaPost: null, error: null});
+})
+
+app.post('/app', function (req, res) {
   let number = req.body.number;
   let url = `http://numbersapi.com/${number}`
 
